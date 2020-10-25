a1595df9238af565b2b30d6a1b4cc1a1c8346fea
Roman Timashev <roman@tmshv.ru>
2020-10-25 20:06:35 +0300

    create random opla scene not just a boxes

--- src/app.ts
+++ src/app.ts
@@ -31,14 +31,57 @@ function applyVertexColors(geometry, color) {
     geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
 }
 
-function createBoxes() {
+function createBoxes(opla: any) {
     let matrix = new THREE.Matrix4();
     let quaternion = new THREE.Quaternion();
     let color = new THREE.Color();
 
-    let result = []
+    // let result = []
+
+    const { grid, items } = opla
+    const result = items.map(item => {
+        const { location } = item
+        let gridScale = 100
+        var position = new THREE.Vector3();
+        position.x = location.x * gridScale
+        position.y = location.y * gridScale
+        position.z = location.z * gridScale
+        // position.x = Math.random() * 10000 - 5000;
+        // position.y = Math.random() * 6000 - 3000;
+        // position.z = Math.random() * 8000 - 4000;
+
+        var rotation = new THREE.Euler();
+        // rotation.x = Math.random() * 2 * Math.PI;
+        // rotation.y = Math.random() * 2 * Math.PI;
+        // rotation.z = Math.random() * 2 * Math.PI;
+
+        gridScale = 10
+        var scale = new THREE.Vector3();
+        // scale.x = Math.random() * 200 + 100;
+        // scale.y = Math.random() * 200 + 100;
+        // scale.z = Math.random() * 200 + 100;
+        scale.x = grid.axisX[location.x] * gridScale
+        scale.y = grid.axisX[location.y] * gridScale
+        scale.z = grid.axisX[location.z] * gridScale
+        let geometry = new THREE.BoxBufferGeometry();
+
+        quaternion.setFromEuler(rotation);
+        matrix.compose(position, quaternion, scale);
+
+        geometry.applyMatrix4(matrix);
+
+        // give the geometry's vertices a random color, to be displayed
+        applyVertexColors(geometry, color.setHex(Math.random() * 0xffffff));
+        return {
+            geometry,
+            position,
+            rotation,
+            scale,
+        }
+    })
 
     for (let i = 0; i < 50; i++) {
+        continue
         let geometry = new THREE.BoxBufferGeometry();
 
         var position = new THREE.Vector3();
@@ -74,7 +117,58 @@ function createBoxes() {
     return result
 }
 
-function createScene(conf: any) {
+function createOplaBlocks() {
+    const items = createGrid3(10, 10, 10, (x, y, z, i) => {
+        if (Math.random() < 0.95) {
+            return null
+        }
+
+        return {
+            id: i,
+            location: {
+                x,
+                y,
+                z,
+            },
+            props: {
+                // width: 10,
+                // height: 10,
+                // depth: 10,
+            }
+        }
+    })
+    const grid = {
+        axisX: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+        axisY: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+        axisZ: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+    }
+
+    return {
+        grid,
+        items,
+    }
+}
+
+type GridFactoryFunction3<T> = (x: number, y: number, z: number, i: number) => T | null
+function createGrid3<T>(x: number, y: number, z: number, factory: GridFactoryFunction3<T>): T[] {
+    const result: T[] = []
+
+    let count = 0
+    for (let i = 0; i < x; i++) {
+        for (let j = 0; j < y; j++) {
+            for (let k = 0; k < z; k++) {
+                const item = factory(i, j, k, count)
+                if (item) {
+                    result.push(item)
+                }
+                count++
+            }
+        }
+    }
+    return result
+}
+
+function createScene(conf: { opla: any }) {
     pickingScene = new THREE.Scene();
     pickingTexture = new THREE.WebGLRenderTarget(1, 1);
 
@@ -83,7 +177,7 @@ function createScene(conf: any) {
 
     var color = new THREE.Color();
 
-    let boxes = createBoxes()
+    let boxes = createBoxes(conf.opla)
     let geometriesDrawn = boxes.map(({ geometry }) => geometry)
     let geometriesPicking = [];
     boxes.forEach((item, i) => {
@@ -126,7 +220,11 @@ function init() {
     light.position.set(0, 500, 2000);
     scene.add(light);
 
-    let { objects, pickingObjects } = createScene({})
+    const opla = createOplaBlocks()
+
+    let { objects, pickingObjects } = createScene({
+        opla
+    })
     scene.add(objects)
     pickingScene.add(pickingObjects)
 
