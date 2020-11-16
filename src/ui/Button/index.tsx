14c097446e47d6df6d3e2521ab1d392f22d25c59
Roman Timashev <roman@tmshv.ru>
2020-11-16 21:53:32 +0300

    Button component theme

--- src/ui/Button/index.tsx
+++ src/ui/Button/index.tsx
@@ -1,14 +1,21 @@
 export type ButtonProps = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
-    // theme?: 'default' | 'primary' | 'link'
+    theme?: 'default' | 'primary' | 'link' | 'checked'
     // size?: ControlsSize
     // shape?: ButtonShape
     // href?: string
     // underlineRef?: MutableRefObject<null>
 }
 
-export const Button: React.FC<ButtonProps> = props => (
+// ['focus:outline-none focus:shadow-outline'],
+const themes = new Map([
+    ['default', 'text-white bg-gray-900 hover:bg-gray-800'],
+    ['checked', 'text-white bg-gray-500 hover:bg-gray-400'],
+    ['primary', ''],
+])
+
+export const Button: React.FC<ButtonProps> = ({ theme = 'default', ...props }) => (
     <button
-        className="bg-gray-900 hover:bg-gray-800 text-white py-1 px-2 focus:outline-none focus:shadow-outline"
+        className={`${themes.get(theme)} py-1 px-2 focus:outline-none focus:shadow-outline`}
         {...props}
     />
 )
