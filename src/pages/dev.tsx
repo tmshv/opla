d3d20ac5e5ba9ab6ed80a27bbcf5b08bb7d40c73
Roman Timashev <roman@tmshv.ru>
2020-11-16 17:24:12 +0300

    dev page upd: size selectors and other changes

--- src/pages/dev.tsx
+++ src/pages/dev.tsx
@@ -1,8 +1,9 @@
 import { runApp } from "@/app"
 import { AppController } from "@/app/controller"
 import { createOplaSystem } from "@/opla"
+import { Button } from "@/ui/Button"
 import { NextPage } from "next"
-import { MutableRefObject, useCallback, useEffect, useRef } from "react"
+import { forwardRef, MutableRefObject, useCallback, useEffect, useRef } from "react"
 
 function make() {
     const opla = createOplaSystem()
@@ -10,28 +11,70 @@ function make() {
     return new AppController(opla)
 }
 
-function useOpla(ref: MutableRefObject<HTMLDivElement>) {
+type AxisSizeSelectorOnClick = (value: number) => void
+type AxisSizeSelectorProps = {
+    onClick: AxisSizeSelectorOnClick
+}
+
+const AxisSizeSelector = forwardRef<HTMLDivElement, AxisSizeSelectorProps>((props, ref) => {
+    const onClick = (e) => {
+        const v = parseInt(e.target.name)
+
+        props.onClick(v)
+    }
+
+    return (
+        <div ref={ref as any}>
+            <Button name={'1'} onClick={onClick}>1</Button>
+            <Button name={'2'} onClick={onClick}>2</Button>
+            <Button name={'3'} onClick={onClick}>3</Button>
+            <Button name={'4'} onClick={onClick}>4</Button>
+            <Button name={'5'} onClick={onClick}>5</Button>
+            <Button name={'6'} onClick={onClick}>6</Button>
+        </div>
+    )
+})
+
+function useOpla(ref: MutableRefObject<HTMLDivElement>, options: any) {
     const ctrl = useRef(make())
 
     useEffect(() => {
         ctrl.current = runApp(ctrl.current, ref.current)
+
     }, [ref.current, ctrl])
 
-    return ctrl
+    return ctrl.current
 }
 
 const Page: NextPage = () => {
     const ref = useRef<HTMLDivElement>()
-    const ctrl = useOpla(ref)
+    const refAxisX = useRef<HTMLDivElement>()
+    const refAxisY = useRef<HTMLDivElement>()
+    const refAxisZ = useRef<HTMLDivElement>()
+    const ctrl = useOpla(ref, {
+        refAxisX,
+        refAxisY,
+        refAxisZ,
+    })
 
     const onClickSelect = useCallback(event => {
-        ctrl.current.setTool('select', {})
+        ctrl.setTool('select', {})
     }, [])
     const onClickAdd = useCallback(event => {
-        ctrl.current.setTool('add', {})
+        ctrl.setTool('add', {})
     }, [])
     const onClickRemove = useCallback(event => {
-        ctrl.current.setTool('remove', {})
+        ctrl.setTool('remove', {})
+    }, [])
+
+    const onClickX = useCallback<AxisSizeSelectorOnClick>(size => {
+        console.log('x', size)
+    }, [])
+    const onClickY = useCallback<AxisSizeSelectorOnClick>(size => {
+        console.log('y', size)
+    }, [])
+    const onClickZ = useCallback<AxisSizeSelectorOnClick>(size => {
+        console.log('z', size)
     }, [])
 
     return (
@@ -45,9 +88,24 @@ const Page: NextPage = () => {
                     bottom: 0,
                 }}
             >
-                <button onClick={onClickSelect}>select</button>
-                <button onClick={onClickAdd}>add</button>
-                <button onClick={onClickRemove}>remove</button>
+                <div className="max-w-sm px-2 py-2 bg-white rounded-none overflow-hidden shadow-lg">
+                    <Button onClick={onClickSelect}>select</Button>
+                    <Button onClick={onClickAdd}>add</Button>
+                    <Button onClick={onClickRemove}>remove</Button>
+
+                    <AxisSizeSelector
+                        onClick={onClickX}
+                        ref={refAxisX}
+                    />
+                    <AxisSizeSelector
+                        onClick={onClickY}
+                        ref={refAxisY}
+                    />
+                    <AxisSizeSelector
+                        onClick={onClickZ}
+                        ref={refAxisZ}
+                    />
+                </div>
             </div>
         </div>
     )
