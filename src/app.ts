580cb881ce2041ca35d909656a7122286e2b4b9b
Roman Timashev <roman@tmshv.ru>
2020-11-16 23:11:16 +0300

    three controls creator moved to separate file

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ import { ScenePicker } from './lib/pick'
 import { AppController } from './app/controller';
 import { loadAssets } from './lib/assets';
 import { OplaCursor } from './lib/cursor';
+import { createControls } from './lib/three';
 
 var container, stats;
 var camera;
@@ -434,49 +435,6 @@ function animate() {
 
     render();
     // stats.update();
-}
-
-function createControls(camera: THREE.Camera, target: HTMLElement) {
-    // controls = new TrackballControls(camera, renderer.domElement);
-    // controls.rotateSpeed = 1.0;
-    // controls.zoomSpeed = 1.2;
-    // controls.panSpeed = 0.8;
-    // controls.noZoom = false;
-    // controls.noPan = false;
-    // controls.staticMoving = true;
-    // controls.dynamicDampingFactor = 0.3;
-
-    const controls = new OrbitControls(camera, target)
-    controls.target.set(0, 1, 0)
-    controls.target.set(
-        -157.3899553540499,
-        110.17876538143562,
-        53.34107147450705,
-    )
-    controls.minDistance = 800
-    controls.maxDistance = 2000
-    // minZoom: number;
-    // maxZoom: number;
-    // controls.minPolarAngle = Math.PI / 2
-    // minPolarAngle: number;
-    // maxPolarAngle: number;
-    // controls.minAzimuthAngle = Math.PI
-    // maxAzimuthAngle: number;
-    // enableDamping: boolean;
-    // dampingFactor: number;
-    controls.enableZoom = true
-    // zoomSpeed: number;
-    // enableRotate: boolean;
-    // rotateSpeed: number;
-    controls.enablePan = true
-    // panSpeed: number;
-    // screenSpacePanning: boolean;
-    // keyPanSpeed: number;
-    controls.autoRotate = false
-    controls.autoRotateSpeed = 0.3
-    controls.update()
-
-    return controls
 }
 
 function nextBlockCell(grid: OplaGrid, dir: string, def: BlockDef) {
