c3076e8c668ae1a359ebeda126d20c8875027e90
Roman Timashev <roman@tmshv.ru>
2023-01-24 13:26:27 +0600

    move walls to component

--- src/components/opla.tsx
+++ src/components/opla.tsx
@@ -535,6 +535,50 @@ const OplaScene: React.FC<OplaSceneProps> = () => {
     )
 }
 
+const Walls: React.FC = () => {
+    const s = 4
+    return (
+        <group name="walls">
+            {/* normal to Y */}
+            <mesh
+                rotation={[-Math.PI / 2, 0, 0]}
+                position={[1.5, -0.5 - 0.07, 1.5]}
+            >
+                <planeGeometry args={[s, s, s]} />
+                <meshStandardMaterial
+                    color={0xaaffaa}
+                    metalness={0.5}
+                    roughness={0.75}
+                />
+            </mesh>
+            {/* normal to X */}
+            <mesh
+                rotation={[0, Math.PI / 2, 0]}
+                position={[-0.5 - 0.07, 1.5, 1.5]}
+            >
+                <planeGeometry args={[s, s, s]} />
+                <meshStandardMaterial
+                    color={0xffaaaa}
+                    metalness={0.5}
+                    roughness={0.75}
+                />
+            </mesh>
+            {/* normal to Z */}
+            <mesh
+                rotation={[0, 0, Math.PI / 2]}
+                position={[1.5, 1.5, -0.5 - 0.07]}
+            >
+                <planeGeometry args={[s, s, s]} />
+                <meshStandardMaterial
+                    color={0xaaaaff}
+                    metalness={0.5}
+                    roughness={0.75}
+                />
+            </mesh>
+        </group>
+    )
+}
+
 export default function Opla() {
     return (
         <Canvas
@@ -558,44 +602,7 @@ export default function Opla() {
             {/*     position={[10, 10, 10]} */}
             {/* /> */}
 
-            <group name="walls">
-                {/* normal to Y */}
-                <mesh
-                    rotation={[-Math.PI / 2, 0, 0]}
-                    position={[1.5, -0.5 - 0.07, 1.5]}
-                >
-                    <planeGeometry args={[4, 4, 4]} />
-                    <meshStandardMaterial
-                        color={0xaaffaa}
-                        metalness={0.5}
-                        roughness={0.75}
-                    />
-                </mesh>
-                {/* normal to X */}
-                <mesh
-                    rotation={[0, Math.PI / 2, 0]}
-                    position={[-0.5 - 0.07, 1.5, 1.5]}
-                >
-                    <planeGeometry args={[4, 4, 4]} />
-                    <meshStandardMaterial
-                        color={0xffaaaa}
-                        metalness={0.5}
-                        roughness={0.75}
-                    />
-                </mesh>
-                {/* normal to Z */}
-                <mesh
-                    rotation={[0, 0, Math.PI / 2]}
-                    position={[1.5, 1.5, -0.5 - 0.07]}
-                >
-                    <planeGeometry args={[4, 4, 4]} />
-                    <meshStandardMaterial
-                        color={0xaaaaff}
-                        metalness={0.5}
-                        roughness={0.75}
-                    />
-                </mesh>
-            </group>
+            <Walls />
         </Canvas>
     )
 }
