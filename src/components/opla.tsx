2e48c31bae0d6e4ee8b1f31b35e9b55a9c479e77
Roman Timashev <roman@tmshv.ru>
2023-01-25 16:48:24 +0600

    move state to separate file

--- src/components/opla.tsx
+++ src/components/opla.tsx
@@ -4,13 +4,14 @@ import { Suspense, useCallback, useState } from "react"
 import { Canvas, MeshProps, useFrame, useThree } from "@react-three/fiber"
 import { Edges, Environment, OrbitControls, useCursor, useGLTF } from "@react-three/drei"
 import { Box3, BoxGeometry, Color, Group, Line3, Mesh, Vector3 } from "three"
-import { proxy, useSnapshot } from "valtio"
+import { useSnapshot } from "valtio"
 import { useControls } from "leva"
 import { SnapTransformControls, TransformSnap } from "./snap-transform-controls"
 import { floor, isInt } from "@/lib/math"
 import { Walls } from "./walls"
 import { pairs } from "@/lib/array"
 import { isIntersects } from "@/lib/t"
+import { state } from "@/state"
 
 type Edge = [Vector3, Vector3]
 
@@ -20,57 +21,6 @@ const edgeNames = new Map([
     [3, "edge_600mm"],
     [4, "edge_800mm"],
 ])
-
-type State = {
-    target: string | null,
-    items: OplaBox[],
-}
-
-let state = proxy<State>({
-    target: null,
-    items: [
-        {
-            id: "1x 1y 1z",
-            position: [0, 0, 0],
-            size: [1, 1, 1],
-        },
-        {
-            id: "2x 1y 1z",
-            position: [0.5, 1, 0],
-            size: [2, 1, 1],
-        },
-        {
-            id: "1x 2y 1z",
-            position: [0, 2.5, 0],
-            size: [1, 2, 1],
-        },
-        {
-            id: "1x 1y 2z",
-            position: [2, 0, 0.5],
-            size: [1, 1, 2],
-        },
-        {
-            id: "1x 2y 3z",
-            position: [2, 1.5, 1],
-            size: [1, 2, 3],
-        },
-        {
-            id: "2x 2y 4z",
-            position: [4.5, 0.5, 1.5],
-            size: [2, 2, 4],
-        },
-        {
-            id: "3x 3y 3z",
-            position: [8, 1, 1],
-            size: [3, 3, 3],
-        },
-        {
-            id: "cube",
-            position: [0, 4, 0],
-            size: [1, 1, 1],
-        },
-    ],
-})
 
 type BoxProps = MeshProps & {
     width: number
@@ -190,12 +140,6 @@ function isOutOfBounds(obj: Mesh): boolean {
     //     console.log("out of bounds", x, y, z, x - w / 2, y - h / 2, z - d / 2)
     // }
     return out
-}
-
-type OplaBox = {
-    id: string
-    position: [number, number, number]
-    size: [number, number, number]
 }
 
 type BoxesProps = {
---
+++ src/state.ts
@@ -0,0 +1,59 @@
+import { proxy } from "valtio"
+
+type OplaBox = {
+    id: string
+    position: [number, number, number]
+    size: [number, number, number]
+}
+
+export type State = {
+    target: string | null,
+    items: OplaBox[],
+}
+
+export let state = proxy<State>({
+    target: null,
+    items: [
+        {
+            id: "1x 1y 1z",
+            position: [0, 0, 0],
+            size: [1, 1, 1],
+        },
+        {
+            id: "2x 1y 1z",
+            position: [0.5, 1, 0],
+            size: [2, 1, 1],
+        },
+        {
+            id: "1x 2y 1z",
+            position: [0, 2.5, 0],
+            size: [1, 2, 1],
+        },
+        {
+            id: "1x 1y 2z",
+            position: [2, 0, 0.5],
+            size: [1, 1, 2],
+        },
+        {
+            id: "1x 2y 3z",
+            position: [2, 1.5, 1],
+            size: [1, 2, 3],
+        },
+        {
+            id: "2x 2y 4z",
+            position: [4.5, 0.5, 1.5],
+            size: [2, 2, 4],
+        },
+        {
+            id: "3x 3y 3z",
+            position: [8, 1, 1],
+            size: [3, 3, 3],
+        },
+        {
+            id: "cube",
+            position: [0, 4, 0],
+            size: [1, 1, 1],
+        },
+    ],
+})
+
