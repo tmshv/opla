20f5c28a237c60a156801b6a98938e8d82edeb4c
Roman Timashev <roman@tmshv.ru>
2023-01-25 14:40:03 +0600

    fix test

--- src/lib/array.test.ts
+++ src/lib/array.test.ts
@@ -3,7 +3,7 @@ import { choise } from "./array"
 // to be sure jest works as expected
 describe("array", () => {
     test("should get one value", () => {
-        const result = choise(["a", "b", "c"], () => 1)
+        const result = choise(["a", "b", "c"], () => 1/3)
         expect(result).toBe("b")
     })
 })
