ee13501ad4009a03c504aeba6ddebf5b8720e04c
Roman Timashev <roman@tmshv.ru>
2023-01-25 16:43:58 +0600

    add test to isIntersects

--- src/lib/t.test.ts
+++ src/lib/t.test.ts
@@ -1,5 +1,5 @@
-import { Box3, Vector3 } from "three"
-import { boxInclusiveIntersect } from "./t"
+import { Box3, BoxGeometry, Group, Mesh, Vector3 } from "three"
+import { boxInclusiveIntersect, isIntersects } from "./t"
 
 describe("three", () => {
     test("no intersection between two adjacent Box3", () => {
@@ -44,6 +44,42 @@ describe("three", () => {
             new Vector3(-0.5, -0.5, -0.5),
         )
         expect(c.containsBox(d)).toBeTruthy()
+    })
+
+    test("should return false if object has no intersection with group", () => {
+        const a = new Mesh(new BoxGeometry(1, 1, 1))
+        a.position.set(0, 0, 0)
+
+        const b = new Mesh(new BoxGeometry(1, 1, 1))
+        b.position.set(1, 0, 0)
+
+        const c = new Mesh(new BoxGeometry(1, 1, 1))
+        c.position.set(0, 1, 0)
+
+        const g = new Group()
+        g.add(a)
+        g.add(b)
+        g.add(c)
+
+        expect(isIntersects(a, g)).toBeFalsy()
+    })
+
+    test("should return true if object has intersection with group", () => {
+        const a = new Mesh(new BoxGeometry(2, 2, 2))
+        a.position.set(0, 0, 0)
+
+        const b = new Mesh(new BoxGeometry(1, 1, 1))
+        b.position.set(1, 0, 0)
+
+        const c = new Mesh(new BoxGeometry(1, 1, 1))
+        c.position.set(0, 1, 0)
+
+        const g = new Group()
+        g.add(a)
+        g.add(b)
+        g.add(c)
+
+        expect(isIntersects(a, g)).toBeTruthy()
     })
 })
 
