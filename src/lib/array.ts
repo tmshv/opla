756083d83c6516fdfe44441d58c102437deaa0a0
Roman Timashev <roman@tmshv.ru>
2023-01-27 15:24:46 +0600

    rewrite pairs without set

--- src/lib/array.ts
+++ src/lib/array.ts
@@ -6,24 +6,13 @@ export function choise<T>(values: T[], random?: () => number): T {
 
 /*
 * create unique list of all possible pairs
-* TODO: rewrite without set
 */
 export function pairs<T>(items: readonly T[]): [T, T][] {
-    const pairs = []
-    const visit = new Set<string>()
+    const pairs: [T, T][] = []
     const len = items.length
     for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len; j++) {
-            if (i === j) {
-                continue
-            }
-            // AB is the same as BA
-            if (visit.has(`${i}-${j}`) || visit.has(`${j}-${i}`)) {
-                continue
-            }
+        for (let j = i+1; j < len; j++) {
             pairs.push([items[i], items[j]])
-            visit.add(`${i}-${j}`)
-            visit.add(`${j}-${i}`)
         }
     }
     return pairs
