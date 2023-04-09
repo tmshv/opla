5f4e0e2004f5656d9ffb8550903ee4f32e603a7b
Roman Timashev <roman@tmshv.ru>
2023-04-09 21:09:25 +0530

    add leva

--- src/pages/dev.tsx
+++ src/pages/dev.tsx
@@ -1,8 +1,12 @@
 import Opla from "@/components/opla"
+import { Leva } from "leva"
 
 const Page = () => {
     return (
-        <Opla />
+        <>
+            <Opla />
+            <Leva flat collapsed />
+        </>
     )
 }
 
