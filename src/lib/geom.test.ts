3818911c317637aebbf9b9ad4e5bd3de7d6da1cc
Roman Timashev <roman@tmshv.ru>
2023-04-15 22:20:48 +0530

    add test for boxToPlanes

--- src/lib/geom.test.ts
+++ src/lib/geom.test.ts
@@ -1,5 +1,5 @@
 import { Box3, Line3, Vector3 } from "three"
-import { boxToLines, boxToVerticies, uniqueVectors } from "./geom"
+import { boxToLines, boxToPlanes, boxToVerticies, uniqueVectors } from "./geom"
 
 describe("uniqueVectors", () => {
     test("should return same list if it empty", () => {
@@ -73,3 +73,21 @@ describe("boxToLines", () => {
     })
 })
 
+describe("boxToPlanes", () => {
+    test("should create 6 edges from one Box3", () => {
+        const box = new Box3(new Vector3(1, 2, 3), new Vector3(4, 5, 6))
+        const result = [
+            [[4, 2, 3], [4, 5, 6]],
+            [[1, 2, 3], [1, 5, 6]],
+            [[1, 2, 3], [4, 2, 6]],
+            [[1, 5, 3], [4, 5, 6]],
+            [[1, 2, 3], [4, 5, 3]],
+            [[1, 2, 6], [4, 5, 6]],
+        ].map(([minArr, maxArr]) => {
+            const min = (new Vector3()).fromArray(minArr)
+            const max = (new Vector3()).fromArray(maxArr)
+            return new Box3(min, max)
+        })
+        expect(boxToPlanes(box)).toEqual(result)
+    })
+})
