ed9616009d972da998c52efe760fa07c3794ca8a
Roman Timashev <roman@tmshv.ru>
2023-04-18 02:17:30 +0530

    add opla store

--- src/stores/opla.ts
+++ src/stores/opla.ts
@@ -1,78 +1,59 @@
 import { proxy } from "valtio"
 
+export type V3 = [number, number, number]
+export type OplaId = string
+
 export type OplaBox = {
-    id: string
-    position: [number, number, number]
-    size: [number, number, number]
+    id: OplaId
+    type: "box"
+    position: V3
+    size: V3
 }
 
+export type OplaGroup = {
+    id: OplaId
+    type: "group"
+    position: V3
+    children: OplaId[]
+}
+
+export type OplaObject =
+    | OplaBox
+    | OplaGroup
+
 export type State = {
-    // target: string | null,
-    items: OplaBox[],
+    version: string
+    items: Record<OplaId, OplaObject>
+    scene: OplaId[]
 }
 
 export let state = proxy<State>({
-    // target: null,
-    items: [
-        {
+    version: "1",
+    scene: ["group-1", "3x3x3"],
+    items: {
+        "1x 1y 1z": {
             id: "1x 1y 1z",
-            position: [0, 0, 0],
+            type: "box",
+            position: [-0.5, -0.5, 0],
             size: [1, 1, 1],
         },
-        {
+        "2x 1y 1z": {
             id: "2x 1y 1z",
-            position: [0.5, 1, 0],
+            type: "box",
+            position: [0, 0.5, 0],
             size: [2, 1, 1],
         },
-        {
-            id: "1x 2y 1z",
-            position: [0, 2.5, 0],
-            size: [1, 2, 1],
-        },
-        {
-            id: "1x 1y 2z",
-            position: [2, 0, 0.5],
-            size: [1, 1, 2],
-        },
-        {
-            id: "1x 2y 3z",
-            position: [2, 1.5, 1],
-            size: [1, 2, 3],
-        },
-        {
-            id: "2x 2y 4z",
-            position: [4.5, 0.5, 1.5],
-            size: [2, 2, 4],
-        },
-        {
-            id: "3x 3y 3z",
-            position: [8, 1, 1],
+        "3x3x3": {
+            id: "3x3x3",
+            type: "box",
+            position: [8, 1, 2],
             size: [3, 3, 3],
         },
-        {
-            id: "4x4x4",
-            position: [1.5, 1.5, 7.5],
-            size: [4, 4, 4],
+        "group-1": {
+            id: "group-1",
+            type: "group",
+            position: [0.5, 0.5, 0],
+            children: ["1x 1y 1z", "2x 1y 1z"],
         },
-        {
-            id: "cube",
-            position: [0, 4, 0],
-            size: [1, 1, 1],
-        },
-        {
-            id: "cube-2",
-            position: [1, 4, 0],
-            size: [1, 1, 1],
-        },
-        {
-            id: "cube-3",
-            position: [2, 4, 0],
-            size: [1, 1, 1],
-        },
-        {
-            id: "cube-4",
-            position: [3, 4, 0],
-            size: [1, 1, 1],
-        },
-    ],
+    },
 })
