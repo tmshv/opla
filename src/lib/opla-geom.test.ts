c12f1631c5fc70a3b59a4fe21c80b16dc7eefb83
Roman Timashev <roman@tmshv.ru>
2023-07-05 15:13:25 +0300

    add sizeToBox3 test

--- src/lib/opla-geom.test.ts
+++ src/lib/opla-geom.test.ts
@@ -1,6 +1,6 @@
 import { Box3, Vector3 } from "three"
 import { OplaBox } from "@/stores/opla"
-import { oplaItemToBox3 } from "./opla-geom"
+import { oplaItemToBox3, sizeToBox3 } from "./opla-geom"
 
 describe("oplaItemToBox3", () => {
     test("should return correct Box3", () => {
@@ -14,3 +14,17 @@ describe("oplaItemToBox3", () => {
         expect(oplaItemToBox3(item)).toEqual(result)
     })
 })
+
+describe("sizeToBox3", () => {
+    test("should return correct Box3", () => {
+        const item: OplaBox = {
+            id: "cube",
+            type: "box",
+            position: [0, 4, 0],
+            size: [1, 1, 1],
+        }
+        const result = new Box3(new Vector3(-0.5, -0.5, -0.5), new Vector3(0.5, 0.5, 0.5))
+        expect(sizeToBox3(item.size)).toEqual(result)
+    })
+})
+
