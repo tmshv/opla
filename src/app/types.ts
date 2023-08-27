5ef38d141fc422cb29dc1bf0d8443c9ace4ef20e
Roman Timashev <roman@tmshv.ru>
2023-08-27 18:00:35 +0300

    rm deprecated

--- src/app/types.ts
+++
@@ -1,6 +0,0 @@
-import * as THREE from "three"
-
-export interface IAssetLibrary {
-    createAsset(name: string): THREE.Object3D | null
-    getAssetNameBySize(name: string, value: number): string
-}
