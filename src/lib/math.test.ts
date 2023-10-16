7d69299462c96a5d2a7f5a1df5f5f68aa1c7f0e3
Roman Timashev <roman@tmshv.ru>
2023-10-16 23:55:23 +0300

    fix math test

--- src/lib/math.test.ts
+++ src/lib/math.test.ts
@@ -2,11 +2,11 @@ import { lerp } from "./math"
 
 describe("lerp", () => {
     test("should return start for 0", () => {
-        expect(lerp(5, 10, 0.5)).toBe(5)
+        expect(lerp(5, 10, 0)).toBe(5)
     })
 
     test("should return end for 1", () => {
-        expect(lerp(5, 10, 0.5)).toBe(10)
+        expect(lerp(5, 10, 1)).toBe(10)
     })
 
     test("should return mid for 0.5", () => {
