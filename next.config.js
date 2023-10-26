748f2f95397b54e1972d87fa581eb1cc1fb6a85b
Roman Timashev <roman@tmshv.ru>
2023-10-27 01:27:26 +0300

    format

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 import analyzer from "@next/bundle-analyzer"
 
 const withBundleAnalyzer = analyzer({
-    enabled: process.env.ANALYZE === 'true',
+    enabled: process.env.ANALYZE === "true",
 })
 
 /**
