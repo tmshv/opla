872ce6cbff2f3c060b7b30f2568536dc15e0db59
Roman Timashev <roman@tmshv.ru>
2023-11-01 16:56:46 +0300

    migrations

---
+++ migrations/1698586541_updated_oplas.go
@@ -0,0 +1,55 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models/schema"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("ezef6sf4s3vzpvt")
+		if err != nil {
+			return err
+		}
+
+		// add
+		new_cover := &schema.SchemaField{}
+		json.Unmarshal([]byte(`{
+			"system": false,
+			"id": "dtpqcimw",
+			"name": "cover",
+			"type": "file",
+			"required": false,
+			"presentable": false,
+			"unique": false,
+			"options": {
+				"maxSelect": 1,
+				"maxSize": 5242880,
+				"mimeTypes": [],
+				"thumbs": [],
+				"protected": false
+			}
+		}`), new_cover)
+		collection.Schema.AddField(new_cover)
+
+		return dao.SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("ezef6sf4s3vzpvt")
+		if err != nil {
+			return err
+		}
+
+		// remove
+		collection.Schema.RemoveField("dtpqcimw")
+
+		return dao.SaveCollection(collection)
+	})
+}
